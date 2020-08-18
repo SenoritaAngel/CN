bd3f3e75eaa34f8d023ae365a2083e22d80a5b44
Isabelle Baert <webrelationpro@gmail.com>
2020-08-18 17:16:31 +0200

    add bullet animation in showSneaks function js

--- source/javascripts/sneak_slide.js
+++ source/javascripts/sneak_slide.js
@@ -2,7 +2,7 @@ var sneakIndex = 1;
 showSneaks(sneakIndex);
 
 // Next/previous controls
-function plusSneaks(n) {
+function plusSneak(n) {
   showSneaks(sneakIndex += n);
 }
 
@@ -14,15 +14,42 @@ function currentSneak(n) {
 function showSneaks(n) {
   var i;
   var sneaks = document.getElementsByClassName("slide-s-screen");
-  var dots = document.getElementsByClassName("dot");
+  var bullets = document.getElementsByClassName("bullet");
   if (n > sneaks.length) {sneakIndex = 1}
   if (n < 1) {sneakIndex = sneaks.length}
   for (i = 0; i < sneaks.length; i++) {
       sneaks[i].style.display = "none";
   }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
+  for (i = 0; i < bullets.length; i++) {
+      bullets[i].className = bullets[i].className.replace(" active", "");
   }
   sneaks[sneakIndex-1].style.display = "flex";
-  dots[sneakIndex-1].className += " active";
+  bullets[sneakIndex-1].className += " active";
 }
+
+/*$(document).ready(function(){
+  // Activate Carousel
+  $("#myCarousel").carousel({interval: 1500});
+
+  // Enable Carousel Indicators
+  $(".item1").click(function(){
+    $("#myCarousel").carousel(0);
+  });
+  $(".item2").click(function(){
+    $("#myCarousel").carousel(1);
+  });
+  $(".item3").click(function(){
+    $("#myCarousel").carousel(2);
+  });
+  $(".item4").click(function(){
+    $("#myCarousel").carousel(3);
+  });
+
+  // Enable Carousel Controls
+  $(".left").click(function(){
+    $("#myCarousel").carousel("prev");
+  });
+  $(".right").click(function(){
+    $("#myCarousel").carousel("next");
+  });
+});*/
