fafa6aedd91958459058210b14a3701f9dd00106
Isabelle Baert <webrelationpro@gmail.com>
2020-08-20 17:36:18 +0200

    remove code to display slideshow

--- source/javascripts/modal_carousel.js
+++ source/javascripts/modal_carousel.js
@@ -8,7 +8,7 @@ function closeModal() {
   document.getElementById("myModal").style.display = "none";
 }
 
-var slideIndex = 1;
+/*var slideIndex = 1;
 showSlides(slideIndex);
 
 // Next/previous controls
@@ -25,16 +25,19 @@ function showSlides(n) {
   var i;
   var slides = document.getElementsByClassName("Slides");
   var dots = document.getElementsByClassName("dot");
-  //var captionText = document.getElementById("caption");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
+
+  if (n > slides.length) {
+    slideIndex = 1
+  }
+  if (n < 1) {
+    slideIndex = slides.length
+  }
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
-  for (i = 0; i < dots.length; i++) {
+  /*for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
   }
   slides[slideIndex-1].style.display = "flex";
   dots[slideIndex-1].className += " active";
-  // captionText.innerHTML = dots[slideIndex-1].alt;
 }
